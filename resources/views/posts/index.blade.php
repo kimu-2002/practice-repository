79b2474fe1f964b2e2bef48ea72507cd6e66d836
kimu-2002 <manato1112manato@icloud.com>
2023-08-09 13:07:15 +0900

    カレッジカリキュラム８－６

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,10 +16,23 @@
             @foreach($posts as $post)
             <div class="posts">
                <a href="/posts/ {{ $post->id }}"><h2 class="title">{{ $post->title }}</h2></a>
-                <p class="body">{{ $post->body }}</p>
+               <p class="body">{{ $post->body }}</p>
+               <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+               <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                   @csrf
+                   @method("DELETE")
             </div>
             @endforeach
         </div>
         <div class="paginate">{{ $posts->links()}}</div>
+        <script>
+    function deletePost(id) {
+        'use strict'
+
+        if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+            document.getElementById(`form_${id}`).submit();
+        }
+    }
+</script>
     </body>
 </html>
